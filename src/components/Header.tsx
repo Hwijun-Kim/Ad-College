fe61670e6f97cf7193858c176e19994f627f744a
김휘준 <kimhwijun@gimhwijun-ui-MacBookPro.local>
2025-02-11 13:39:48 +0900

    오타 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
 import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
